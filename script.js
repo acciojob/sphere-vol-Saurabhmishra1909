8e53813805961a274d48791f28c3398660bfd643
Saurabh Mishra <84716420+Saurabhmishra1909@users.noreply.github.com>
2025-03-20 20:22:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 function volume_sphere() {
-    //Write your code here
-  
-} 
+    //Wvrite your code here
+	const radii=document.getElementById('radius');
+	const vol=document.getElementById('volume');
+	const radius=parseFloat(radii.value);
+	if(isNaN(radius)||radius<0){
+		vol.value='NaN';
+		return;
+	}
+	const volume=(4/3)*Math.PI*Math.pow(radius,3);
+	vol.value=volume.toFixed(4);
+}  
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+window.onload = function () {
+  document.getElementById('MyForm').onsubmit = function (event) {
+    event.preventDefault(); // Prevent the form from submitting
+    volume_sphere(); // Call the volume_sphere function
+  };
+};
